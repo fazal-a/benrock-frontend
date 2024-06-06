7e715755f21b4f51535a48121ef1b69a2034066d
Fazal Abbas <fazal.a@pacsquare.com>
2024-06-06 16:09:02 +0500

    updated feed page

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,3 +1,4 @@
+/*eslint-disable*/
 import { Avatar, Col, Divider, Image, Modal, Row, Skeleton } from 'antd'
 import moment from 'moment'
 import React, { useEffect, useRef, useState } from 'react'
@@ -10,10 +11,11 @@ import { imageBaseUrl, privateAPI } from '../config/constants'
 import Layout from '../layout/Layout'
 import { addClick, getNearestAttachments, getRecentAttachments } from '../services/Attachments'
 import { getContacts } from '../services/chat'
+import { AiFillLike } from 'react-icons/ai'
 
 const Feed = () => {
   const chatSelector = useSelector((state) => state.chat)
-  const [loading, setLoading] = useState(false)
+  // const [loading, setLoading] = useState(false)
   const [feed, setFeed] = useState([])
   const [popular, setPopular] = useState([])
   const user = useSelector((state) => state.auth.user)
@@ -22,39 +24,120 @@ const Feed = () => {
   const [videoModalVisible, setVideoModalVisible] = useState(false)
   const [selectedVideo, setSelectedVideo] = useState(null)
 
-  const [posts, setPosts] = useState([]);
-  const [page, setPage] = useState(1);
-  const [hasMore, setHasMore] = useState(true);
-  const loader = useRef(null);
+  const [loading, setLoading] = useState(false)
+
+  const [posts, setPosts] = useState([])
+  const [page, setPage] = useState(1)
+  const [hasMore, setHasMore] = useState(true)
+  const loader = useRef(null)
+  useEffect(() => {
+    fetchRecentPosts(page)
+  }, [page])
 
   useEffect(() => {
-    const observer = new IntersectionObserver(handleObserver,
-      {
-        root: null,
-        rootMargin: "20px",
-        threshold: 1.0
-      });
-    if (loader.current){
-      observer.observe(loader.current);
-    }
-  }, []);
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPage((prevPage) => prevPage + 1)
+        }
+      },
+      { threshold: 1.0 },
+    )
 
-  const loadPosts = async () => {
-    const response = await privateAPI.get(`/attachment/getPaginatedAttachments?page=${page}&limit=3`);
-    const data = await response.data;
-    setPosts(prev => [...prev, ...data?.data]);
-    setHasMore(data?.page < data?.pages);
-  };
-  const handleObserver = (entities) => {
-    const target = entities[0];
-    if (target.isIntersecting && hasMore) {
-      setPage((prev) => prev + 1);
+    if (loader.current) {
+      observer.observe(loader.current)
     }
-  };
+
+    return () => {
+      if (loader.current) {
+        observer.unobserve(loader.current)
+      }
+    }
+  }, [loader, hasMore])
+
+  const fetchRecentPosts = async () => {
+    setLoading(true)
+    try {
+      const response = await privateAPI.get(
+        `/attachment/getPaginatedAttachments?page=${page}&limit=3`,
+      )
+      const data = await response.data
+      setPosts((prev) => [...prev, ...data?.data])
+      setHasMore(data?.page < data?.pages)
+    } catch (error) {
+      console.error('Failed to load posts', error)
+    }
+    setLoading(false)
+  }
+
+  const [popularPosts, setPopularPosts] = useState([])
+  const [popularPostsPage, setPopularPostsPage] = useState(1)
+  const [hasMorePopularPosts, setHasMorePopularPosts] = useState(true)
+  const popularPostsLoader = useRef(null)
 
   useEffect(() => {
-    loadPosts();
-  }, [page]);
+    fetchPopularPosts(popularPostsPage)
+  }, [popularPostsPage])
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && hasMorePopularPosts) {
+          setPopularPostsPage((prevPage) => prevPage + 1)
+        }
+      },
+      { threshold: 1.0 },
+    )
+
+    if (popularPostsLoader.current) {
+      observer.observe(popularPostsLoader.current)
+    }
+
+    return () => {
+      if (popularPostsLoader.current) {
+        observer.unobserve(popularPostsLoader.current)
+      }
+    }
+  }, [popularPostsLoader, hasMorePopularPosts])
+
+  const fetchPopularPosts = async () => {
+    setLoading(true)
+    try {
+      const response = await privateAPI.get(
+        `/attachment/getPopularAttachments?page=${popularPostsPage}&limit=3`,
+      )
+      const data = await response.data
+      setPopularPosts((prev) => [...prev, ...data?.data])
+      setHasMorePopularPosts(data?.page < data?.pages)
+    } catch (error) {
+      console.error('Failed to load posts', error)
+    }
+    setLoading(false)
+  }
+
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver(handleObserver,
+  //     {
+  //       root: null,
+  //       rootMargin: "20px",
+  //       threshold: 1.0
+  //     });
+  //   if (loader.current){
+  //     observer.observe(loader.current);
+  //   }
+  // }, []);
+
+  //
+  // const handleObserver = (entities) => {
+  //   const target = entities[0];
+  //   if (target.isIntersecting && hasMore) {
+  //     setPage((prev) => prev + 1);
+  //   }
+  // };
+  //
+  // useEffect(() => {
+  //   loadPosts();
+  // }, [page]);
 
   // Function to handle click on video thumbnail
   const handleVideoThumbnailClick = (video) => {
@@ -65,27 +148,27 @@ const Feed = () => {
   const [activeTab, setActiveTab] = useState('Recent')
   const navigate = useNavigate()
 
-  useEffect(() => {
-    getContacts()
-    getFeedData()
-  }, [])
+  // useEffect(() => {
+  //   getContacts()
+  //   getFeedData()
+  // }, [])
 
-  useEffect(() => {
-    if (activeTab === 'Recent') getFeedData()
-    else getNearest()
-  }, [activeTab])
+  // useEffect(() => {
+  //   if (activeTab === 'Recent') getFeedData()
+  //   else getNearest()
+  // }, [activeTab])
 
-  const getFeedData = async () => {
-    setLoading(true)
-    let response = await getRecentAttachments()
-    // console.log({ response })
-    if (response?.status === 200) {
-      setLoading(false)
-      setFeed(response?.data?.data)
-    } else {
-      setLoading(false)
-    }
-  }
+  // const getFeedData = async () => {
+  //   setLoading(true)
+  //   let response = await getRecentAttachments()
+  //   // console.log({ response })
+  //   if (response?.status === 200) {
+  //     setLoading(false)
+  //     setFeed(response?.data?.data)
+  //   } else {
+  //     setLoading(false)
+  //   }
+  // }
 
   const getNearest = async () => {
     setLoading(true)
@@ -105,6 +188,7 @@ const Feed = () => {
       },
     })
   }
+
   const NoFeeds = () => {
     return (
       <div className='noFeeds'>
@@ -130,27 +214,36 @@ const Feed = () => {
     if (loading) {
       return <Skeleton active avatar style={{ margin: 10 }} />
     }
-
     return (
-      <div className="card">
-        <div className="card-header">
-          <Avatar
-            style={{ outline: '1px solid var(--gray300)' }}
-            src={post?.createdBy?.photo ? `${imageBaseUrl}/${post?.createdBy?.photo}` : logo}
-            size={50}
-          ></Avatar>
-          <div className="name-date">
-            <h2 className="userName">{post?.createdBy?.name}</h2>
-            <div className="timeaGo">
-              {moment(post?.createdAt).isSame(moment(), 'day')
-                ? moment(post?.createdAt).fromNow()
-                : moment(post?.createdAt).format('DD MMM, YYYY')}
+      <div className='card'>
+        <div className='card-header'>
+          <div>
+            <Avatar
+              style={{ outline: '1px solid var(--gray300)' }}
+              src={post?.createdBy?.photo ? `${imageBaseUrl}/${post?.createdBy?.photo}` : logo}
+              size={50}
+            ></Avatar>
+
+            <div className='name-date'>
+              <h2 className='userName'>{post?.createdBy?.name}</h2>
+
+              <div className='timeaGo'>
+                {moment(post?.createdAt).isSame(moment(), 'day')
+                  ? moment(post?.createdAt).fromNow()
+                  : moment(post?.createdAt).format('DD MMM, YYYY')}
+              </div>
+            </div>
+            <div>
+              <AiFillLike
+                style={{ color: '#1890ff', cursor: 'pointer', marginLeft: 10 }}
+                onClick={onClick}
+              />
             </div>
           </div>
         </div>
 
-        <div className="card-content">
-          {post?.type === 'photo' ?
+        <div className='card-content'>
+          {post?.type === 'photo' ? (
             <div style={{ display: 'flex', justifyContent: 'center' }}>
               <Image
                 src={`${imageBaseUrl}/${post?.path}`}
@@ -160,10 +253,9 @@ const Feed = () => {
                 onClick={() => onClick({ ...post, createdBy: { _id: post._id } })}
               />
             </div>
-
-            :
+          ) : (
             <video
-              className="feedVideos"
+              className='feedVideos'
               src={`${imageBaseUrl}/${post?.path}`}
               controls
               autoPlay
@@ -174,84 +266,7 @@ const Feed = () => {
                 onClick({ ...post, createdBy: { _id: post._id } })
               }}
             />
-          }
-          {/* Ant Design Modal for Videos */}
-          <Modal
-            title=""
-            open={videoModalVisible}
-            onCancel={() => setVideoModalVisible(false)}
-            footer={null}
-            centered
-          >
-            {selectedVideo && (
-              <video controls width="100%" height="400px">
-                <source src={`${imageBaseUrl}/${selectedVideo.path}`} />
-                Your browser does not support the video tag.
-              </video>
-            )}
-          </Modal>
-        </div>
-      </div>
-    )
-
-  }
-  const SingleCard = ({ data, onClick }) => {
-    if (loading) {
-      return <Skeleton active avatar style={{ margin: 10 }} />
-    }
-
-    return (
-      <div className="card single-card">
-        <div className="card-header">
-          <Avatar
-            style={{ outline: '1px solid var(--gray300)' }}
-            src={data?.createdBy?.photo ? `${imageBaseUrl}/${data?.createdBy?.photo}` : logo}
-            size={50}
-          ></Avatar>
-          <div className="name-date">
-            <h2 className="userName">{data?.createdBy?.name}</h2>
-            {/* <p className='cardDate'>
-              {moment(data?.attachments?.createdAt).format('DD MMM, YYYY')}
-            </p> */}
-            <div className='timeaGo'>
-              {moment(data?.createdAt).isSame(moment(), 'day')
-                ? moment(data?.createdAt).fromNow()
-                : moment(data?.createdAt).format('DD MMM, YYYY')}
-            </div>
-          </div>
-        </div>
-        <div className='card-content'>
-          {data?.type == 'photo' ? (
-            <>
-              <div>
-                <Image
-                  src={`${imageBaseUrl}/${data?.path}`}
-                  width={'100%'}
-                  height={'500px'}
-                  style={{ objectFit: 'cover' }}
-                  onClick={() => onClick(data)}
-                />
-              </div>
-            </>
-          ) : (
-            <>
-              <div>
-                <video
-                  className='feedVideos'
-                  src={`${imageBaseUrl}/${data?.path}`}
-                  controls
-                  autoPlay
-                  loop
-                  muted
-                  onClick={() => {
-                    handleVideoThumbnailClick(data)
-                    onClick(data)
-                  }}
-                />
-              </div>
-            </>
           )}
-
           {/* Ant Design Modal for Videos */}
           <Modal
             title=''
@@ -296,69 +311,35 @@ const Feed = () => {
             </span>
           </span>
         </div>
-            {activeTab === 'Recent' ? (
-              <div className="leftFeedSection">
-                {posts?.map((post) => (<Card post={post} key={post?.id} onClick={onAttachmentClick} />))}
-                <div ref={loader} />
+        {activeTab === 'Recent' ? (
+          <>
+            {posts.length > 0 ? (
+              <div className='leftFeedSection'>
+                {posts?.map((post) => (
+                  <Card post={post} key={post?._id} onClick={onAttachmentClick} />
+                ))}
+                {loading && <Skeleton active avatar style={{ margin: 10 }} />}
+                <div ref={loader} style={{ height: '20px' }} />
               </div>
             ) : (
-              <div className='leftFeedSection'>
-                {popular?.length > 0 ? (
-                  popular?.map((value, index) => (
-                    <SingleCard data={value} key={index} onClick={onAttachmentClick} />
-                  ))
-                ) : (
-                  <NoFeeds />
-                )
-                }
-              </div>
+              <NoFeeds />
             )}
-          {/*  <div className='rightFeedSection'>*/}
-          {/*    <div className='profileCard'>*/}
-          {/*      <div style={{ border: '1px solid var(--textColor)', borderRadius: '50%' }}>*/}
-          {/*        <Avatar*/}
-          {/*          src={user?.user?.photo ? `${imageBaseUrl}/${user?.user?.photo}` : logo}*/}
-          {/*          size={80}*/}
-          {/*        ></Avatar>*/}
-          {/*      </div>*/}
-          {/*      <div>*/}
-          {/*        <h2 className='profileName'>{user?.user?.name}</h2>*/}
-          {/*        <p className='profileEmail'>{user?.user?.email}</p>*/}
-          {/*      </div>*/}
-          {/*      <button onClick={() => navigate('/profile')}>*/}
-          {/*        <span>Profile</span>*/}
-          {/*      </button>*/}
-          {/*    </div>*/}
-          {/*     <div className='PROFILE_DIVIDER'>*/}
-          {/*      <Divider />*/}
-          {/*    </div>*/}
-          {/*    <div className='feedChat'>*/}
-          {/*      <h2>Chats</h2>*/}
-          {/*      <Divider></Divider>*/}
-          {/*      {chatSelector?.contacts?.map((contact, index) => {*/}
-          {/*        return (*/}
-          {/*          <div*/}
-          {/*            key={index}*/}
-          {/*            className='chatContent'*/}
-          {/*            onClick={() => handleNavigateToChat({ ...contact, id: contact?._id })}*/}
-          {/*          >*/}
-          {/*            <div style={{ border: '1px solid var(--gray300)', borderRadius: '50%' }}>*/}
-          {/*              <Avatar*/}
-          {/*                src={*/}
-          {/*                  <img*/}
-          {/*                    src={contact?.photo ? `${imageBaseUrl}/${contact?.photo}` : logo}*/}
-          {/*                    alt='avatar'*/}
-          {/*                  />*/}
-          {/*                }*/}
-          {/*                size={30}*/}
-          {/*              ></Avatar>*/}
-          {/*            </div>*/}
-          {/*            <p className='chatUser'>{contact?.name}</p>*/}
-          {/*          </div>*/}
-          {/*        )*/}
-          {/*      })}*/}
-          {/*    </div>*/}
-          {/*  </div>*/}
+          </>
+        ) : (
+          <>
+            {popularPosts.length > 0 ? (
+              <div className='leftFeedSection'>
+                {popularPosts?.map((popularPost) => (
+                  <Card post={popularPost} key={popularPost?._id} onClick={onAttachmentClick} />
+                ))}
+                {loading && <Skeleton active avatar style={{ margin: 10 }} />}
+                <div ref={popularPostsLoader} style={{ height: '20px' }} />
+              </div>
+            ) : (
+              <NoFeeds />
+            )}
+          </>
+        )}
       </div>
     </Layout>
   )
